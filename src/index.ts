be7a3ba56fff4d01f9c392818fce196efe18e2f3
Samuel Gratzl <samuel_gratzl@gmx.at>
2016-10-26 22:32:07 +0200

    fixing quad tree rendering with clip

--- src/index.ts
+++ src/index.ts
@@ -68,6 +68,7 @@ export function circleSymbol(fillStyle:string = 'steelblue', size = 20):ISymbol<
 
   return (ctx:CanvasRenderingContext2D) => {
     //before
+    ctx.fillStyle = fillStyle;
     ctx.beginPath();
     return {
       //during
@@ -78,7 +79,6 @@ export function circleSymbol(fillStyle:string = 'steelblue', size = 20):ISymbol<
       //after
       done: () => {
         ctx.closePath();
-        ctx.fillStyle = fillStyle;
         ctx.fill();
       }
     };
@@ -169,7 +169,7 @@ export default class Scatterplot<T> {
     select(this.canvas).call(this.zoom);
 
     //generate a quad tree out of the data
-    this.tree = quadtree(data, this.options.x, this.options.x);
+    this.tree = quadtree(data, this.options.x, this.options.y);
   }
 
   checkResize() {
@@ -198,14 +198,22 @@ export default class Scatterplot<T> {
     this.xscale.range([bounds.x0, bounds.x1]);
     this.yscale.range([bounds.y1, bounds.y0]);
 
-    this.renderAxes();
+    //transform scale
+    const transform = zoomTransform(this.canvas);
+    const xscale = transform.rescaleX(this.xscale);
+    const yscale = transform.rescaleY(this.yscale);
+
+    this.renderAxes(xscale, yscale);
 
     ctx.clearRect(0, 0, c.width, c.height);
+
     ctx.save();
-    //ctx.rect(bounds.x0, bounds.y0, bounds.x1 - bounds.x0, bounds.y1 - bounds.y0);
-    //ctx.clip();
-    //get current transform
-    this.renderPoints(ctx, bounds);
+
+    ctx.rect(bounds.x0, bounds.y0, bounds.x1 - bounds.x0, bounds.y1 - bounds.y0);
+    ctx.clip();
+
+    this.renderPoints(ctx, xscale, yscale, bounds);
+
     ctx.restore();
   }
 
@@ -213,76 +221,77 @@ export default class Scatterplot<T> {
     this.render();
   }
 
-  private renderAxes() {
-    const transform = zoomTransform(this.canvas);
-    const left = axisLeft(transform.rescaleY(this.yscale)),
-      bottom = axisBottom(transform.rescaleY(this.xscale)),
+  private renderAxes(xscale: IScale, yscale: IScale) {
+    const left = axisLeft(yscale),
+      bottom = axisBottom(xscale),
       $parent = select(this.parent);
     $parent.select('svg g').call(left);
     $parent.select('svg:last-of-type g').call(bottom);
   }
 
-  private renderPoints(ctx:CanvasRenderingContext2D, bounds: {x0: number, y0: number, x1: number, y1: number}) {
-    const transform = zoomTransform(this.canvas);
+  private renderPoints(ctx:CanvasRenderingContext2D, xscale: IScale, yscale: IScale, bounds: {x0: number, y0: number, x1: number, y1: number}) {
     const {x, y} = this.options;
-    const tx = (d:number) => transform.x + transform.k * this.xscale(d);
-    const ty = (d:number) => transform.y + transform.k * this.yscale(d);
-
     function debugNode(color: string, x0: number, y0: number, x1: number, y1: number) {
       ctx.closePath();
       ctx.fillStyle = 'steelblue';
       ctx.fill();
       ctx.fillStyle = color;
-      ctx.fillRect(tx(x0),ty(y0),tx(x1)-tx(x0),ty(y1)-ty(y0));
+      x0 = xscale(x0);
+      y0 = yscale(y0);
+      x1 = xscale(x1);
+      y1 = yscale(y1);
+      ctx.fillRect(Math.min(x0,x1),Math.min(y0,y1),Math.abs(x0-x1),Math.abs(y0-y1));
       ctx.beginPath();
 
     }
 
     const dbounds = {
-      x0: this.xscale.invert((bounds.x0-transform.x)/transform.k),
-      x1: this.xscale.invert((bounds.x1-transform.x)/transform.k),
+      x0: xscale.invert(bounds.x0),
+      x1: xscale.invert(bounds.x1),
       //since y domain is inverted
-      y1: this.yscale.invert((bounds.y0-transform.y)/transform.k),
-      y0: this.yscale.invert((bounds.y1-transform.y)/transform.k),
+      y1: yscale.invert(bounds.y0),
+      y0: yscale.invert(bounds.y1),
     };
 
     function isVisible(x0: number, y0: number, x1: number, y1: number) {
-      //convert to target space
-      //x0 = tx(x0);
-      //x1 = tx(x1);
-      //y0 = ty(y0);
-      //y1 = ty(y1);
-      //y scale is inverted in the output domain, so swap
-      //[y0, y1] = [y1, y0];
-
       //if the 1er points are small than 0er or 0er bigger than 1er than outside
       if (x1 < dbounds.x0 || y1 < dbounds.y0 || x0 > dbounds.x1 || y0 > dbounds.y1) {
-        debugNode('rgba(255,0,0,0.2)', x0, y0, x1, y1);
-        return true;
+        //debugNode('rgba(255,0,0,0.2)', x0, y0, x1, y1);
+        return false;
       }
-      debugNode('rgba(0,255,0,0.2)', x0, y0, x1, y1);
+      //debugNode('rgba(0,255,0,0.2)', x0, y0, x1, y1);
       //inside or partial overlap
       return true;
     }
 
     ctx.save();
+
     //quad tree based iteration
     const renderer = this.symbol(ctx);
+
     this.tree.visit((node:QuadtreeInternalNode<T> | QuadtreeLeaf<T>, x0: number, y0: number, x1: number, y1: number) => {
       if (!(<any>node).length) { //is a leaf
         var leaf = <QuadtreeLeaf<T>>node;
         //see https://github.com/d3/d3-quadtree
         do {
           let d = leaf.data;
-          renderer.render(tx(x(d)), ty(y(d)), d);
+          renderer.render(xscale(x(d)), yscale(y(d)), d);
+          //console.log(x(d), y(d), '=',xscale(x(d)), yscale(y(d)));
         } while ((leaf = leaf.next) != null);
         return true; //don't visit
       } else {
+        //console.log(x0,y0,x1,y1, '=',xscale(x0), yscale(y0), xscale(x1), yscale(y1));
         //negate, since true means not visit
         return !isVisible(x0, y0, x1, y1);
       }
     });
     renderer.done();
+
+    //a dummy path to clear the 'to draw' state
+    ctx.beginPath();
+    ctx.closePath();
+
+    ctx.restore();
   }
 }
 
