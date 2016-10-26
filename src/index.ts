8328af3c6e4b8157712f897d54c2564b19d38c97
Samuel Gratzl <samuel_gratzl@gmx.at>
2016-10-26 23:53:28 +0200

    primitive tooltip

--- src/index.ts
+++ src/index.ts
@@ -12,8 +12,8 @@ export interface IScale extends AxisScale<number>, ZoomScale {
   range(range:number[]);
   range(): number[];
   domain(): number[];
-  domain(domain: number[]);
-  invert(v: number): number;
+  domain(domain:number[]);
+  invert(v:number): number;
   copy(): IScale;
 }
 
@@ -55,6 +55,8 @@ export interface IScatterplotOptions<T> {
 
   clickRadius?: number;
 
+  tooltipDelay?: number;
+  tooltip?(d: T): string;
 }
 
 /**
@@ -85,13 +87,17 @@ export default class Scatterplot<T> {
     clickRadius: 10,
     scaleExtent: [1 / 2, 4],
     x: (d) => (<any>d).x,
-    y: (d) => (<any>d).y
+    y: (d) => (<any>d).y,
+    tooltipDelay: 300,
+    tooltip: (d) => JSON.stringify(d)
   };
 
   private zoom = zoom().on('zoom', this.onZoom.bind(this)).on('end', this.onZoomEnd.bind(this));
 
   private tree:Quadtree<T>;
-  private _selection : Quadtree<T>;
+  private _selection:Quadtree<T>;
+
+  private showTooltipHandle = -1;
 
   constructor(data:T[], private parent:HTMLElement, options?:IScatterplotOptions<T>) {
     this.options = merge(this.options, options);
@@ -115,7 +121,9 @@ export default class Scatterplot<T> {
       .scaleExtent(this.options.scaleExtent);
     select(this.canvas)
       .call(this.zoom)
-      .on('click', () => this.onClick(d3event));
+      .on('click', () => this.onClick(d3event))
+      .on('mouseleave', () => this.onMouseLeave(d3event))
+      .on('mousemove', () => this.onMouseMove(d3event));
 
     //generate a quad tree out of the data
     this.tree = quadtree(data, this.options.x, this.options.y);
@@ -142,7 +150,7 @@ export default class Scatterplot<T> {
     return this._selection.data();
   }
 
-  set selection(selection: T[]) {
+  set selection(selection:T[]) {
     if (selection.length === 0) {
       if (this._selection.size() === 0) {
         return;
@@ -172,7 +180,7 @@ export default class Scatterplot<T> {
     }
   }
 
-  addToSelection(items: T[]) {
+  addToSelection(items:T[]) {
     if (items.length === 0) {
       return;
     }
@@ -180,7 +188,7 @@ export default class Scatterplot<T> {
     this.render();
   }
 
-  removeFromSelection(items: T[]) {
+  removeFromSelection(items:T[]) {
     if (items.length === 0) {
       return;
     }
@@ -192,14 +200,14 @@ export default class Scatterplot<T> {
     const transform = zoomTransform(this.canvas);
     const xscale = transform.rescaleX(this.xscale);
     const yscale = transform.rescaleY(this.yscale);
-    return { xscale, yscale };
+    return {xscale, yscale};
   }
 
   render() {
     const c = this.canvas,
       ctx = this.ctx,
       margin = this.options.margin,
-      bounds = { x0: margin.left, y0: margin.top, x1: c.clientWidth - margin.right, y1: c.clientHeight - margin.bottom};
+      bounds = {x0: margin.left, y0: margin.top, x1: c.clientWidth - margin.right, y1: c.clientHeight - margin.bottom};
     this.checkResize();
 
     this.xscale.range([bounds.x0, bounds.x1]);
@@ -232,15 +240,12 @@ export default class Scatterplot<T> {
     this.render();
   }
 
-  private onClick(event: MouseEvent) {
-    const pos = mouse(this.canvas);
-
-    const { xscale, yscale} = this.transformedScales();
-    const dataPos = [xscale.invert(pos[0]), yscale.invert(pos[1])];
+  private onClick(event:MouseEvent) {
+    const {x, y} = this.getMouseDataPos();
 
     //find closest data item
     //TODO implement a find all to select more than one item
-    const closest = this.tree.find(dataPos[0], dataPos[1], this.options.clickRadius);
+    const closest = this.tree.find(x, y, this.options.clickRadius);
     if (closest) {
       this.selection = [closest];
     } else {
@@ -248,7 +253,31 @@ export default class Scatterplot<T> {
     }
   }
 
-  private renderAxes(xscale: IScale, yscale: IScale) {
+  private getMouseDataPos(pos = mouse(this.canvas)) {
+    const {xscale, yscale} = this.transformedScales();
+    return {x: xscale.invert(pos[0]), y: yscale.invert(pos[1])};
+  }
+
+  private showTooltip(pos: [number, number]) {
+    const {x, y} = this.getMouseDataPos(pos);
+    const item = this.tree.find(x, y, this.options.clickRadius);
+    this.canvas.title = item ? this.options.tooltip(item) : '';
+  }
+
+  private onMouseMove(event:MouseEvent) {
+    const that = this;
+    //clear old
+    clearTimeout(this.showTooltipHandle);
+    const pos = mouse(this.canvas);
+    this.showTooltipHandle = setTimeout(this.showTooltip.bind(this, pos), this.options.tooltipDelay);
+  }
+
+  private onMouseLeave(event: MouseEvent) {
+    clearTimeout(this.showTooltipHandle);
+    this.showTooltipHandle = -1;
+  }
+
+  private renderAxes(xscale:IScale, yscale:IScale) {
     const left = axisLeft(yscale),
       bottom = axisBottom(xscale),
       $parent = select(this.parent);
@@ -256,11 +285,11 @@ export default class Scatterplot<T> {
     $parent.select('svg:last-of-type g').call(bottom);
   }
 
-  private renderPoints(ctx:CanvasRenderingContext2D, xscale: IScale, yscale: IScale, bounds: {x0: number, y0: number, x1: number, y1: number}) {
+  private renderPoints(ctx:CanvasRenderingContext2D, xscale:IScale, yscale:IScale, bounds:{x0: number, y0: number, x1: number, y1: number}) {
     const {x, y} = this.options;
-    var renderer: ISymbolRenderer<T> = null;
+    var renderer:ISymbolRenderer<T> = null;
 
-    function debugNode(color: string, x0: number, y0: number, x1: number, y1: number) {
+    function debugNode(color:string, x0:number, y0:number, x1:number, y1:number) {
       ctx.closePath();
       ctx.fillStyle = 'steelblue';
       ctx.fill();
@@ -269,7 +298,7 @@ export default class Scatterplot<T> {
       y0 = yscale(y0);
       x1 = xscale(x1);
       y1 = yscale(y1);
-      ctx.fillRect(Math.min(x0,x1),Math.min(y0,y1),Math.abs(x0-x1),Math.abs(y0-y1));
+      ctx.fillRect(Math.min(x0, x1), Math.min(y0, y1), Math.abs(x0 - x1), Math.abs(y0 - y1));
       ctx.beginPath();
 
     }
@@ -282,7 +311,7 @@ export default class Scatterplot<T> {
       y0: yscale.invert(bounds.y1),
     };
 
-    function isVisible(x0: number, y0: number, x1: number, y1: number) {
+    function isVisible(x0:number, y0:number, x1:number, y1:number) {
       //if the 1er points are small than 0er or 0er bigger than 1er than outside
       if (x1 < dbounds.x0 || y1 < dbounds.y0 || x0 > dbounds.x1 || y0 > dbounds.y1) {
         //debugNode('rgba(255,0,0,0.2)', x0, y0, x1, y1);
@@ -293,7 +322,7 @@ export default class Scatterplot<T> {
       return true;
     }
 
-    function visitTree(node:QuadtreeInternalNode<T> | QuadtreeLeaf<T>, x0: number, y0: number, x1: number, y1: number) {
+    function visitTree(node:QuadtreeInternalNode<T> | QuadtreeLeaf<T>, x0:number, y0:number, x1:number, y1:number) {
       if (!(<any>node).length) { //is a leaf
         var leaf = <QuadtreeLeaf<T>>node;
         //see https://github.com/d3/d3-quadtree
@@ -329,7 +358,7 @@ export default class Scatterplot<T> {
   }
 
 
-  private renderLasso(ctx: CanvasRenderingContext2D) {
+  private renderLasso(ctx:CanvasRenderingContext2D) {
     ctx.save();
 
     ctx.restore();
