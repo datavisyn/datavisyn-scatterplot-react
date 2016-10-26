46a6d24886e16f21c5219f045951ce506356fe47
Samuel Gratzl <samuel_gratzl@gmx.at>
2016-10-26 16:14:32 +0200

    start with canvas

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 
 
 class CanvasScatterplot {
+  constructor(data, canvas) {
+    this._data = data;
+    this._canvas = canvas;
+  }
+
+  render() {
+    const c = this._canvas;
+
+    const ctx = c.getContext('2d');
+    ctx.clearRect(0, 0, c.width, c.height);
+
+  }
+
 
 }
 
--- test/indexTest.js
+++ test/indexTest.js
@@ -5,14 +5,31 @@
 
 import CanvasScatterplot from '../src';
 
+function randomData(count = 100) {
+  var r = [];
+  for(let i = 0; i < count; ++i) {
+    r.push({ x: Math.random(), y: Math.random()});
+  }
+  return r;
+}
+
+function setupCanvas(width = 100, height = 100) {
+  const c = document.createElement('canvas');
+  c.width = width;
+  c.height = height;
+  return c;
+}
+
 describe('CanvasScatterplot', () => {
-  let m;
+  const data = randomData(100);
+  var cs = null;
 
   beforeEach(() => {
-    m = 'dummy';
+    var canvas = setupCanvas(100, 100);
+    cs = new CanvasScatterplot(data, canvas);
   });
 
   it('dummy', () => {
-    expect(m).to.equal('dummy');
+    expect(cs._canvas).to.be.defined();
   });
 });
