34039d70a2fe293240533fa0f2e0687cc88a2171
Samuel Gratzl <samuel_gratzl@gmx.at>
2016-10-27 05:07:26 +0200

    fix source maps + smaller builds now

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,56 @@
+const webpack = require('webpack');
 const path = require('path');
 
-module.exports = {
-  entry: {
-    vanilla: [path.resolve(__dirname, 'src/index.ts')],
-    react: [path.resolve(__dirname, 'src/react/index.tsx')]
-  },
-  output: {
-    path: path.resolve(__dirname, 'build'),
-    publicPath: '/',
-    filename: '[name].js',
-    libraryTarget: 'umd',
-    library: ['datavisyn', 'scatterplot']
-  },
-  module: {
-    loaders: [{
-      test: /\.tsx?$/,
-      loader: 'awesome-typescript-loader',
-      include: path.resolve(__dirname, 'src')
-    }]
-  },
-  resolve: {
-    extensions: ['.webpack.js', '.web.js', '.js', '.ts', '.tsx'],
+module.exports = function (env) {
+  const isProduction = env === 'prod';
+  const base = {
+    entry: {
+      vanilla: [path.resolve(__dirname, 'src/index.ts')],
+      react: [path.resolve(__dirname, 'src/react/index.tsx')]
+    },
+    output: {
+      path: path.resolve(__dirname, 'build'),
+      publicPath: '/',
+      filename: '[name].js',
+      libraryTarget: 'umd',
+      library: ['datavisyn', 'scatterplot']
+    },
+    module: {
+      loaders: [{
+        test: /\.tsx?$/,
+        loader: 'awesome-typescript-loader',
+        include: path.resolve(__dirname, 'src')
+      }]
+    },
+    resolve: {
+      extensions: ['.webpack.js', '.web.js', '.js', '.ts', '.tsx'],
 
-    modules: [path.resolve(__dirname, 'src'), 'node_modules']
-  },
-  externals: {
-    react: 'React',
-    'react-dom': 'ReactDOM'
-  },
-  devServer: {
-    contentBase: path.resolve(__dirname, 'build')
-  },
-  devtool: 'source-map'
+      modules: [path.resolve(__dirname, 'src'), 'node_modules']
+    },
+    externals: {
+      react: 'React',
+      'react-dom': 'ReactDOM'
+    }, plugins: [],
+    devServer: {
+      contentBase: path.resolve(__dirname, 'build')
+    },
+    devtool: isProduction ? 'hidden-source-map' : 'source-map'
+  };
+  if (isProduction) {
+    base.plugins.push(
+      new webpack.LoaderOptionsPlugin({
+        minimize: true,
+        debug: false
+      }),
+      new webpack.optimize.UglifyJsPlugin({
+        compress: {
+          warnings: false
+        },
+        output: {
+          comments: false
+        },
+        sourceMap: false
+      }));
+  }
+  return base;
 };
