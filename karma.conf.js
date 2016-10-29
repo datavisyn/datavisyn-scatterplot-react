5e932e062c371d80826b6d05be26f9b7ce631f13
Samuel Gratzl <samuel_gratzl@gmx.at>
2016-10-29 04:05:39 +0200

    fix karma config

--- karma.conf.js
+++ karma.conf.js
@@ -1,46 +1,39 @@
 var path = require('path');
 
+var webpack_config = require('./webpack.config.js')('dev');
+delete webpack_config.entry;
+delete webpack_config.external;
+
 // Set node environment to testing
 process.env.NODE_ENV = 'test';
 
-module.exports = function(config) {
-    config.set({
-        basePath: '',
-        browsers: ['Firefox'],
-        files: [
-          'test/loadtests.js'
-        ],
-        port: 8000,
-        captureTimeout: 60000,
-        frameworks: ['mocha', 'chai'],
-        client: {
-          mocha: {}
-        },
-        singleRun: true,
-        reporters: ['mocha', 'coverage'],
-        preprocessors: {
-          'test/loadtests.js': ['webpack']
-        },
-        webpack: {
-          module: {
-            loaders: [{
-              test: /\.tsx?$/,
-              loader: 'awesome-typescript-loader',
-              include: path.resolve(__dirname, 'src/'),
-              loader: 'babel'
-            }],
-            resolve: {
-              extensions: ['', '.webpack.js', '.web.js', '.js', 'ts', '.tsx']
-            },
-            devtool: 'eval'
-          },
-          coverageReporter: {
-            dir: 'coverage/',
-            reporters: [{
-              type: 'html'
-            }, {
-              type: 'text'
-            }]
-          }
-        });
-    };
+module.exports = function (config) {
+  config.set({
+    basePath: '',
+    browsers: ['Firefox'],
+    files: [
+      'test/loadtests.js'
+    ],
+    port: 8000,
+    captureTimeout: 60000,
+    frameworks: ['mocha', 'chai'],
+    client: {
+      mocha: {}
+    },
+    singleRun: true,
+    reporters: ['mocha', 'coverage'],
+    preprocessors: {
+      'test/loadtests.js': ['webpack']
+    },
+    failOnEmptyTestSuite: false,
+    webpack: webpack_config,
+    coverageReporter: {
+      dir: 'coverage/',
+      reporters: [{
+        type: 'html'
+      }, {
+        type: 'text'
+      }]
+    }
+  });
+};
