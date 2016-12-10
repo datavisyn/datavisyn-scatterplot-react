4ff35e48c4f70e7c6911c73af4a8c6b57f901a27
Samuel Gratzl <samuel_gratzl@gmx.at>
2016-12-10 21:26:18 +0100

    fix public path to be relative

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = function (env) {
     },
     output: {
       path: path.resolve(__dirname, 'build'),
-      publicPath: '/',
+      publicPath: './',
       filename: '[name].js',
       libraryTarget: 'umd',
       library: ['datavisyn', 'scatterplot']
