2f059d44bc5d475297f36b11b37832e62abc07c3
Samuel Gratzl <samuel_gratzl@gmx.at>
2016-12-12 18:14:13 +0100

    first brush and significance line for manhattan plot

--- src/ManhattanPlot.tsx
+++ src/ManhattanPlot.tsx
@@ -5,8 +5,9 @@
 import * as React from 'react';
 import {scaleLinear} from 'd3-scale';
 import {axisLeft, axisBottom} from 'd3-axis';
-import {select} from 'd3-selection';
-import * as d3brush from 'd3-brush';
+import {select, event as d3event} from 'd3-selection';
+import {drag} from 'd3-drag';
+import {brushX, D3BrushEvent} from 'd3-brush';
 
 export interface IManhattanPlotProps {
   serverUrl: string;
@@ -25,6 +26,13 @@ interface IChromosome {
   name: string;
   start: number;
   end: number;
+}
+
+function pvalue2sig(pvalue) {
+  return -Math.log(pvalue);
+}
+function sig2pvalue(sig) {
+  return Math.exp(-sig);
 }
 
 
@@ -48,8 +56,8 @@ export default class ManhattanPlotReact extends React.Component<IManhattanPlotPr
 
   private parent: SVGSVGElement = null;
 
-  private xscale = scaleLinear().range([this.props.margin.left, this.props.width-this.props.margin.right]);
-  private yscale = scaleLinear().range([this.props.height-this.props.margin.bottom, this.props.margin.top]);
+  private xscale = scaleLinear().range([this.props.margin.left, this.props.width - this.props.margin.right]);
+  private yscale = scaleLinear().range([this.props.height - this.props.margin.bottom, this.props.margin.top]);
   private xaxis = axisBottom(this.xscale);
   private yaxis = axisLeft(this.yscale);
   private chromosomes: IChromosome[];
@@ -74,6 +82,31 @@ export default class ManhattanPlotReact extends React.Component<IManhattanPlotPr
     const $parent = select(this.parent);
     $parent.select('g.datavisyn-manhattanplot-xaxis').call(this.xaxis);
     $parent.select('g.datavisyn-manhattanplot-yaxis').call(this.yaxis);
+    const margin = this.props.margin;
+
+    const sigline = this.yscale(pvalue2sig(this.props.geqSignificance));
+    const $line_area = $parent.select('rect.datavisyn-manhattanplot-significance-hidden');
+    const $line = $parent.select('line.datavisyn-manhattanplot-significance').call(drag().on('drag', () => {
+      const y = Math.max(margin.top, Math.min(this.props.height-margin.bottom,d3event.y));
+
+      $line.attr('y1', y);
+      $line.attr('y2', y);
+      $line_area.attr('y', y).attr('height', (this.props.height-margin.bottom-y));
+      const sig = this.yscale.invert(y);
+      console.log('significance', sig, sig2pvalue(sig));
+      // TODO propagate
+    })).attr('y1', sigline)
+      .attr('y2', sigline);
+    $line_area.attr('y', sigline).attr('height', (this.props.height-margin.bottom-sigline));
+
+    $parent.select('g.datavisyn-manhattanplot-brush').call(brushX().on('brush', () => {
+      const s : [number, number] = (d3event as D3BrushEvent<any>).selection as any;
+      if (s) {
+        console.log('window', this.xscale.invert(s[0]), this.xscale.invert(s[1]));
+      } else {
+        console.log('none');
+      }
+    }).extent([[margin.left, margin.top], [this.props.width - margin.right, this.props.height - margin.bottom]]));
   }
 
   render() {
@@ -84,14 +117,19 @@ export default class ManhattanPlotReact extends React.Component<IManhattanPlotPr
     };
 
     return <svg ref={(div) => this.parent = div as SVGSVGElement} width={this.props.width} height={this.props.height}>
-      <image x={margin.left} y={margin.top} width={image.width} height={image.height} preserveAspectRatio='none'
-             xlinkHref={`${this.props.serverUrl}/manhattan?plain=true&geqSignificance=${this.props.geqSignificance}&width=${image.width}&height=${image.height}`}>
+      <g style={{pointerEvents: 'none'}}>
+        <g className='datavisyn-manhattanplot-yaxis' transform={`translate(${margin.left},0)`} />
+        <g className='datavisyn-manhattanplot-xaxis' transform={`translate(0,${image.height+margin.top})`} />
+        <rect className='datavisyn-manhattanplot-significance-hidden' x={margin.left} y={margin.top + image.height} width={image.width} height='0' style={{fill: 'rgba(0,0,0,0.1)'}} />
+      </g>
+      <line x1='0' x2='100%' className='datavisyn-manhattanplot-significance' style={{stroke: 'black', strokeWidth: 3, cursor: 'ns-resize'}}/>
 
-      </image>
-      <g className='datavisyn-manhattanplot-yaxis' transform={`translate(${margin.left},0)`}>
-      </g>
-      <g className='datavisyn-manhattanplot-xaxis' transform={`translate(0,${image.height+margin.top})`}>
-      </g>
+      <g className='datavisyn-manhattanplot-brush'></g>
     </svg>;
   }
 }
+
+// <image x={margin.left} y={margin.top} width={image.width} height={image.height} preserveAspectRatio='none'
+//             xlinkHref={`${this.props.serverUrl}/manhattan?plain=true&geqSignificance=${this.props.geqSignificance}&width=${image.width}&height=${image.height}`}>
+//
+//      </image>
